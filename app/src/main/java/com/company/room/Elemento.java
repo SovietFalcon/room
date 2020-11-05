8f971adb606441a756dcc5d63081f97a161d25ed
gerardfp <gerardfp@gmail.com>
2020-11-05 10:53:29 +0100

    Initial commit

---
+++ app/src/main/java/com/company/room/Elemento.java
@@ -0,0 +1,12 @@
+package com.company.room;
+
+public class Elemento {
+    String nombre;
+    String descripcion;
+    float valoracion;
+
+    public Elemento(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+}
